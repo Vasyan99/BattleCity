700432c7f4367f5a198df66600c0355395b3bfd0
Vasyan <vasyan99@mail.ru>
2025-06-16 15:32:43 +0500

    OpenGL and glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,82 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY); // показываем, где хотим рисовать, в какой области окна. В параметры передаём координаты левого нижнего угла окна (0, 0), далее ширина и высота
+//Меняется область, где хотим рисовать в окне
+
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE); //Если нажата клавиша ескейп, закрываем окно
+    }
+}
+
+
+
 int main(void)
 {
-    GLFWwindow* window;
+
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); //хотим использовать версию 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //хотим использовать core profile для opengl
+    //Core profile - определённое подмножество функций opengl куда не включены разные функции обратной совместимости
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr); //размеры, название окна и доп. параметры монитор + другое окно
+    if (!pWindow)
     {
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback); //Регистрируем наш коллбэк. Теперь, когда меняем вручную размер окна - меняются и параметры
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
+
+
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow); //делаем контекст окна для текущего окна. окон может быть несколько для каждого контекст
 	
 	if (!gladLoadGL())
 	{
-		std::cout << "Can't load GLAD" << std::endl;
+		std::cout << "Can't load GLAD!" << std::endl;
 		return -1;
 	}
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
-	
+
 	glClearColor(0, 1, 0, 1);
 	
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow)) //главная Render Loop. Делаем отрисовку на экране
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT); //очищает буффер цвета 
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow); //видеокарта рисует задний буффер, монитор отображает передний. 
+        //Буфферы меняются местами, кадр заднего буффера закончен
 
         /* Poll for and process events */
-        glfwPollEvents();
+        glfwPollEvents(); //Позволяет glfw обработать все события, такие как нажатие клавиши, клик мыши.
     }
 
-    glfwTerminate();
+    glfwTerminate(); //освободить все ресурсы glfw
     return 0;
 }
